579cdc4bb2c85c4f1cbae83602187778ef23df50
Sanjana1508 <sanjana.podduturi@zemosolabs.com>
2021-05-10 18:45:00 +0530

    Assignment10

---
+++ src/Assignments/SList.java
@@ -0,0 +1,75 @@
+package Assignments;
+
+import java.util.Iterator;
+
+
+public class SList<E> {
+	ListNode<E> head;
+	int size;
+	
+	public SList() {
+		head = new ListNode<E>(null);
+		size = 0;
+	}
+	
+	public Iterator<E> iterator(){
+		return new Iterator<E>() {
+          int myCount = 0;
+          ListNode<E> node = head;
+
+            @Override
+            public boolean hasNext () {
+                return myCount < size;
+            }
+
+            @Override
+            public E next () {
+                E s = node.data;
+                myCount++;
+                node=node.next;
+                return s;
+            }
+        };
+	}
+	
+	@Override
+	public String toString() {
+		return "SList [head=" + head + ", size=" + size + "]";
+	}
+	
+	public void insert(int index,E element) {
+		if(index < 0 || index >= size)
+			throw new IndexOutOfBoundsException();
+		if(element == null)
+			throw new NullPointerException();
+		ListNode<E> node = new ListNode(element);
+		ListNode<E> headTemp = head;
+		for(int i = 0;i < index; i++)
+			headTemp = headTemp.next;
+		ListNode<E> nodenext = headTemp.next;
+		headTemp.next = node;
+		node.next = nodenext;
+		size++;
+	}
+	
+	public E remove(int index) {
+		if(index < 0 || index >= size)
+			throw new IndexOutOfBoundsException();
+		ListNode<E> headTemp = head;
+		for(int i = 0;i < index;i++)
+			headTemp = headTemp.next;
+		E oldData = headTemp.next.data;
+		headTemp.next = headTemp.next.next;
+		size--;
+		return oldData;
+	}
+}
+class ListNode<E>{
+	E data;
+	ListNode<E> next;
+	
+	public ListNode(E e) {
+		this.data = e;
+		this.next = null;
+	}
+}
